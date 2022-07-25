2edc6d67976bfc3a07d0db6f44d36617bfd98194
Sajjad Hossain <59442218+sajjad-h@users.noreply.github.com>
2022-07-25 17:03:31 +0600

    Create template.cpp

---
+++ cp/template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define   ll    long long int
+
+const int N = 200005;
+const ll mod = 1000000007;
+
+void testcase()
+{
+
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int t = 1; //cin >> t;
+    while (t--) {
+        testcase();
+    }
+    return 0;
+}
+
+/**
+
+
+
+**/
