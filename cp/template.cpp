64ea95b1baace4c718746343f3ef214f4aacdcda
Sajjad Hossain <59442218+sajjad-h@users.noreply.github.com>
2022-08-01 20:23:17 +0600

    Create template.cpp

--- cp/template.cpp
+++ cp/template.cpp
@@ -16,7 +16,7 @@ int main()
     ios_base::sync_with_stdio(false);cin.tie(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
-    int t = 1; //cin >> t;
+    int t = 1; cin >> t;
     while (t--) {
         testcase();
     }
